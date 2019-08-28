35c9529362840076f64ba1917be40a76030d6696
github username <github email address>
2019-08-28 15:48:51 -0600

    Working Methods

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,16 +1,16 @@
 def get_first_name_of_season_winner(data, season)
   # code here
-  data[season].each do |array|
-    if array["status"] == "Winner"
-      return array["name"].split(" ")[0]
+  data[season].each do |contestant|
+    if contestant["status"] == "Winner"
+      return contestant["name"].split(" ")[0]
     end
   end  
 end
 
 def get_contestant_name(data, occupation)
   # code here
-  data.each do |season, array|
-    array.each do |info|
+  data.each do |season, contestant|
+    contestant.each do |info|
       if info["occupation"] == occupation
         return info["name"]
       end
@@ -21,8 +21,8 @@ end
 def count_contestants_by_hometown(data, hometown)
   # code here
   counter = 0
-  data.each do |season, array|
-    array.each do |info|
+  data.each do |season, contestant|
+    contestant.each do |info|
       if info["hometown"] == hometown
         counter += 1
       end
